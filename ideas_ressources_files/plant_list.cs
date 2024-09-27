b27683efe4cd98a5aa118408d2c5841615e0db65
L-PT-1463 <lucassoaresfonseca08@gmail.com>
2024-09-27 12:50:22 +0200

    Update - Plant_list
    Added plant template and fixed alignement on templates for FlagZ_list and Zombotany_list
    
    This plant template will likely change once developpement of the Plant_list begins

--- ideas_ressources_files/plant_list.cs
+++ ideas_ressources_files/plant_list.cs
@@ -1,6 +1,17 @@
 
 //This is the full list of plants, which will have an almanac in the game. There will be adjustements with play-testing and many of these will take a long to actually join the game. Any ideas are welcome as long as you follow the guidelines according to the comented template. You are welcome to propose changes and give feedback.
 
+    //template_plant
+        string despcription = "";   //basic description of the plant (if available, you can easily take it straight from its game of origin)
+        double cost = 25;           //sun cost of the plant. The game works on a 25 sun basis
+        double hp = 6;              //hit points (hp) are counted in peas. Yes, even the plants'. Note: A regular plant has 6 hp
+        double dmg = 1;             //damage (dmg) is counted in peas
+        double aps = 1;             //aps stands for attacks per second, aka how fast the plant attacks
+        string special = "";        //if relevant, add a precise description of the ability/ies (this won't   show up in the actual almanac, it is instead used for coding), if not remove this line.
+        string almanac = "";        //almanac entry, be it copied from one of the original games or entirely/partially new
+        string origin = "";         //game(s) or mod(s) of origin
+
+
 
 
 
--- ideas_ressources_files/zombotany_list.cs
+++ ideas_ressources_files/zombotany_list.cs
@@ -2,19 +2,19 @@
 //This is the full list of zombotany zombie variants for all the various plants, yes, ALL the various plants. Any plants added to the "plant_list.cs" will be have a unique zombotany.
 
     //template_zombotany
-            string plant_name = "Grass"; //name of the plant
-            string description = "Grass, but dead"; //basic description of the zombie and its abilities (if available, you can easily take it straight from its game of origin)
-            double hp = 10; //hit points (hp) is counted in peas
-            double armour = 0; //if relevant, add the hp of potential armour (i.e. Cone), if not "double armour = 0"
-            int cr = 1; //CR stands for Challenge Rating, being the amount of "points" needed to spawn (+CR = Spawns later/less often) and therefore should be base on how threatening the zombie is. A zombotany specific guide will be made in due time, but winging it usually works too.
-            double speed = normal; //speeds written after template
-            double dmg = 1; //damage (dmg) is counted in peas - yes, even the zombies'
-            double dmg_ranged = 1; //dmg for the ranged attack. Only keep if relevent
-            double aps = 2; //aps stands for attacks per second, aka how fast the zombie attacks
-            double aps_ranged = 2; //aps but for the ranged attack. Only keep if relevent
-            string special = "Suddenly combusts, but only if it feels like it."; //if relevant, add a precise description of the ability/ies (this won't   show up in the actual almanac, it is instead used for coding), if not remove this line. This is also where you include Magnet-shroom weakness. Note : by default, zombies can't spawn on water or on air at all, so add precisions such as "Can spawn" or "only spawns in water" for the pool and "Flies towards the roof" (Flying zombies fly at "roof height", so if a peashooter is placed on the roof it'll be able to shoot the flying zombies) when necessary.
-            string almanac = "Because why wouldn't we add 5000 more almanac entries to the game ?";
-            string origin = "Rogue Garden"; //game(s) or mod(s) of origin - Add the game of origin of the plant, taken from the list and the game of origin of the zombotany if taken from one.
+            string plant_name = "Grass";                                    //name of the plant head
+            string description = "Grass, but dead";                         //basic description of the zombie and its abilities (if available, you can easily take it straight from its game of origin)
+            double hp = 10;                                                 //hit points (hp) is counted in peas
+            double armour = 0;                                              //if relevant, add the hp of potential armour (i.e. Cone), if not "double armour = 0"
+            int cr = 1;                                                     //CR stands for Challenge Rating, being the amount of "points" needed to spawn (+CR = Spawns later/less often) and therefore should be base on how threatening the zombie is. A zombotany specific guide will be made in due time, but winging it usually works too.
+            double speed = normal;                                          //speeds written after template
+            double dmg = 1;                                                 //damage (dmg) is counted in peas - yes, even the zombies'
+            double dmg_ranged = 1;                                          //dmg for the ranged attack. Only keep if relevent
+            double aps = 2;                                                 //aps stands for attacks per second, aka how fast the zombie attacks
+            double aps_ranged = 2;                                          //aps but for the ranged attack. Only keep if relevent
+            string special = "Is very good at dying.";                      //if relevant, add a precise description of the ability/ies (this won't   show up in the actual almanac, it is instead used for coding), if not remove this line. This is also where you include Magnet-shroom weakness. Note : by default, zombies can't spawn on water or on air at all, so add precisions such as "Can spawn" or "only spawns in water" for the pool and "Flies towards the roof" (Flying zombies fly at "roof height", so if a peashooter is placed on the roof it'll be able to shoot the flying zombies) when necessary.
+            string almanac = "Let's double the plant's almanac entries !";
+            string origin = "Rogue Garden";                                 //game(s) or mod(s) of origin - Add the game of origin of the plant, taken from the list and the game of origin of the zombotany if taken from one.
 
 double slower = 1;
 double slow = 2;
